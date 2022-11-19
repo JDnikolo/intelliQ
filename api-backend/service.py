ef428ca2fc637fc06419248a1c1dfd2151a9c910
Giannis Nikolopoulos <el15170@mail.ntua.gr>
2022-11-19 12:12:37 +0200

    MySQL connector configured

--- api-backend/service.py
+++ api-backend/service.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import mysql.connector
+
 
 app = Flask(__name__)
+# TODO move this to another file
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database='intelliq',
+    port=3306
+)
+# sqlcursor.execute(sql_query) to access database
+sqlcursor = mydb.cursor()
 
-##TODO connect to database
 
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET"])
 def hello_world():
-    return "<p>Hello, World!</p>"
-##method parameters can be <variables> 
-@app.route("/<name>",methods=["GET"])
-##variables must be included as keyword arguements
+
+    return f"<p>Hello World!</p>"
+
+
+# method parameters can be <variables>
+
+
+@app.route("/<name>", methods=["GET"])
+# variables must be included as keyword arguements
 def hello_named(name=""):
     return "<p>Hello, {}!</p>".format(name)
