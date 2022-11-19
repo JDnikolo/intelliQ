55c16ee202660d1a775f2eb31412c13b6cc05b78
Giannis Nikolopoulos <el15170@mail.ntua.gr>
2022-11-19 13:32:32 +0200

    Moved config to another file.

--- api-backend/service.py
+++ api-backend/service.py
@@ -3,17 +3,17 @@ import mysql.connector
 from mysqlconfig import *
 
 app = Flask(__name__)
-# TODO move this to another file
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database='intelliq',
-    port=3306
-)
-
+# # TODO move this to another file
+# mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    password="",
+#    database='intelliq',
+#    port=3306
+# )
+#
 # sqlcursor.execute(sql_query) to access database
-sqlcursor = mydb.cursor()
+sqlcursor = myconnector.cursor()
 
 
 @app.route("/", methods=["GET"])
