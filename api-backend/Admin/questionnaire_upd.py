23a4cdc4b527978bcf0d68d27dd088e910030da9
ntua-el19955 <el19955@mail.ntua.gr>
2022-12-09 18:03:04 +0200

    Fixed endpoint questionnaire_upd functionality, minor changes in service.py
    Tested with postman, further error checks might be necessary.

--- api-backend/Admin/questionnaire_upd.py
+++ api-backend/Admin/questionnaire_upd.py
@@ -1,11 +1,8 @@
 from service import myconnector
-from flask import request, Response, jsonify, Blueprint
+from flask import request, Response, Blueprint
 import json
 #TODO: Add administrator access only.
-#TODO: Add remaining bad data checks.
-#TODO: needs to be tested more with Postman, only questionnaire and keyword table can be successfully updated right now
-#after that, error code 500 will be raised 
-#WORK IN PROGRESS
+#TODO: Add remaining bad data checks. 
 
 questionnaire_upd = Blueprint("questionnaire_upd", __name__) 
 
@@ -15,8 +12,8 @@ def admin_questionnaire_upd():
         try:
             sqlcursor = myconnector.cursor(buffered=True)
             #Check if a file was uploaded
-            if 'a' not in request.files:            # use postman post request with form-data, 'a'was the key associated with test json file
-                return Response("No file uploaded",status = 400)
+            if 'a' not in request.files:            # use postman post request with form-data, 'a' is the key associated with test json file
+                return Response("No file uploaded.",status = 400)
             temp = request.files['a']
             q = json.load(temp)                     #parse JSON file
             if q:                                   #get json data
@@ -28,16 +25,13 @@ def admin_questionnaire_upd():
                 sqlcursor.execute("SELECT questionnaireID FROM questionnaire WHERE questionnaireID=%s", [qnID])
                 q_res = sqlcursor.fetchall()                                                
                 if len(q_res) != 0 :             #if it does, return corresponding error
-                    return Response("Questionnaire ID already exists", status=400)
+                    return Response("Questionnaire ID already exists.", status=400)
                 sqlcursor.execute(               #else insert data to db, start with questionnaire table  
                 "INSERT INTO questionnaire (questionnaireID,title) VALUES (%s,%s)", [qnID, qTitle])
-                myconnector.commit()            #TODO commit should only be called if no errors were found during queries?
                 for keyword in keywords:
                     sqlcursor.execute(          #insert keywords on keywords table  
                     "INSERT INTO keywords (questionnaireID,word) VALUES (%s,%s)", [qnID, keyword])
-                    myconnector.commit()
-                #insert questionnaire questions into the database
-                #TODO: fix the following loops, error 500 happens in this for loop block 
+                #insert questionnaire questions into the database 
                 for question in questions:    
                     #TODO Check if there are any badly written strings in current question dictionary
                     qID = question['qID']
@@ -48,8 +42,7 @@ def admin_questionnaire_upd():
                         required = int(0)
                     qtype = question['type']
                     sqlcursor.execute(      #insert current question on questions table  
-                        "INSERT INTO question (questionID,qtext,required,qtype,qnrID) VALUES (%s,%s,%d,%s,%s)", [qID, qtext, required, qtype, qnID])
-                    myconnector.commit()
+                        "INSERT INTO question (questionID,qtext,required,qtype,qnrID) VALUES (%s,%s,%s,%s,%s)", [qID, qtext, required, qtype, qnID])
                 for question in questions:
                     qID = question['qID']
                     #insert options of current question into the database
@@ -57,14 +50,17 @@ def admin_questionnaire_upd():
                             optID = option['optID']
                             text = option['opttxt']
                             nextq = option['nextqID']
-                            sqlcursor.execute(      #inserts current option on option table  
-                                "INSERT INTO qoption (optionID,optionTXT,nextQ,questionID) VALUES (%s,%s,%s,%s)", [optID, text, nextq, qID])
-                            myconnector.commit()
-                #all done, return success message
+                            if nextq == "-" :           #if nextq is "-" (null) adjust query appropriately
+                                sqlcursor.execute(      #inserts current option on option table  
+                                    "INSERT INTO qoption (optionID,optionTXT,questionID) VALUES (%s,%s,%s)", [optID, text, qID])
+                            else:                       
+                                sqlcursor.execute(      #inserts current option on option table  
+                                    "INSERT INTO qoption (optionID,optionTXT,nextQ,questionID) VALUES (%s,%s,%s,%s)", [optID, text, nextq, qID])
+                #all done, commit changes and return success message
                 myconnector.commit()
                 sqlcursor.close()
-                return Response("Questionnaire inserted successfully",status = 200)
+                return Response("Questionnaire inserted successfully.",status = 200)
             else:
-                return Response("Bad file", status = 400)
+                return Response("Bad file.", status = 400)
         except Exception as error:
-            return Response("error", status = 500)
+            return Response("Error.", status = 500)
