5721144f948c96bec40243f788e97f90f2788ba7
ntua-el19156 <el19156@mail.ntua.gr>
2023-01-03 15:19:02 +0200

    getquestionanswers csv and authentication

--- api-backend/Users/getquestionanswers.py
+++ api-backend/Users/getquestionanswers.py
@@ -1,35 +1,81 @@
 from flask import request, Blueprint, jsonify
 from mysqlconfig import myconnector
+from authentication import authUser
+from csvResponse import generateCSVresponse
 
 getquestionanswers_blueprint = Blueprint("getquestionanswers", __name__)
 
 @getquestionanswers_blueprint.route('/getquestionanswers/<questionnaireID>/<questionID>', methods = ['POST', 'GET'])
 def getquestionanswers(questionnaireID,questionID):
     if request.method == 'GET':
-        sqlcursor = myconnector.cursor()
-        sqlcursor.execute(''' SELECT sessionID, IF (STRCMP(`optionTXT`,"<open string>") != 0,
-        ans_optionID, answertxt)
-        FROM Answer INNER JOIN qoption
-        WHERE (optionID = ans_optionID AND questionID = %s AND qnrID = %s)
-        ORDER BY sessionID''',(str(questionID),str(questionnaireID)))
-        result = sqlcursor.fetchall()
-        if len(result) == 0:
+        if authUser():
+            args = request.args
+            if (len(args) == 0):
+                format = "json"
+            elif (len(args) > 1):
+                return jsonify({"type": "/errors/operation-error",
+                            "title": "Invalid query parameters.",
+                            "status": "400",
+                            "detail": "Only format is acceptable query parameter.",
+                            "instance": "/Users/getquestionanswers"}), 400
+            elif (len(args) == 1):
+                temp = args.to_dict()
+                temp = temp.keys()
+                temp = list(temp)
+                temp = temp[0]
+                if (temp != "format"):
+                    return jsonify({"type": "/errors/operation-error",
+                            "title": "Invalid query parameters.",
+                            "status": "400",
+                            "detail": "Only format is acceptable query parameter.",
+                            "instance": "/Users/getquestionanswers"}), 400
+                elif (args.get("format") == "json"):
+                    format = "json"
+                elif (args.get("format") == "csv"):
+                    format = "csv"
+                else:
+                    return jsonify({"type": "/errors/operation-error",
+                            "title": "Invalid format type.",
+                            "status": "400",
+                            "detail": "Only json and csv are acceptable formats.",
+                            "instance": "/Users/getquestionanswers"}), 400
+            sqlcursor = myconnector.cursor()
+            sqlcursor.execute(''' SELECT sessionID, IF (STRCMP(`optionTXT`,"<open string>") != 0,
+            ans_optionID, answertxt)
+            FROM Answer INNER JOIN qoption
+            WHERE (optionID = ans_optionID AND questionID = %s AND qnrID = %s)
+            ORDER BY sessionID''',(str(questionID),str(questionnaireID)))
+            result = sqlcursor.fetchall()
+            if len(result) == 0:
+                #ERROR TYPE????????????????????????????
+                return jsonify({"type": "/errors/operation-error",
+                            "title": "No answers found.",
+                            "status": "402",
+                            "detail": "No answers were found.",
+                            "instance": "/Users/getquestionanswers"}), 402
+            sqlcursor.close()
+            for i in range(len(result)):
+                result[i] = dict(session = result[i][0], ans = result[i][1])
+            if (format == 'json'):
+                return jsonify({"questionnaireID": str(questionnaireID),
+                "questionID": str(questionID), "answers": result}), 200
+            else:
+                newDict = dict(questionnaireID = str(questionnaireID),
+                session = str(questionID), answers = result)
+                return generateCSVresponse(newDict, "answers")
+        else:
             return jsonify({
-                "type":"/errors/not-found",
-                "title": "Not Found",
-                "status": "402",
-                "detail":"No answers found.",
-                "instance":"/getquestionanswers/{}/{}".format(questionnaireID, questionID)}), 402 #404
-        sqlcursor.close()
-        for i in range(len(result)):
-            result[i] = dict(session = result[i][0], ans = result[i][1])
-        return jsonify({"questionnaireID": str(questionnaireID),
-        "questionID": str(questionID), "answers": result}), 200
+                        "type":"/errors/authentication-error",
+                        "title": "Unauthorized User",
+                        "status": "401",
+                        "detail":"User is unauthorized",
+                        "instance":"/Users/getquestionanswers"}), 401
 
     if request.method == 'POST':
-        return jsonify({
-                        "type":"/errors/method-not-allowed",
-                        "title": "Method Not Allowed",
-                        "status": "400",
-                        "detail":"POST Request not allowed.",
-                        "instance":"/getquestionanswers/{}/{}".format(questionnaireID, questionID)}), 400    #405
+        #return jsonify({"status":"failed", "reason":"POST is invalid in getquestionanswers"}), 400
+        #ERROR TYPE????????????????????????????
+        return jsonify({"type": "/errors/operation-error",
+                        "title": "Invalid method.",
+                        "status": "405",
+                        "detail": "POST is invalid in getsessionanswers.",
+                        "instance": "/Users/getquestionanswers"}), 405
