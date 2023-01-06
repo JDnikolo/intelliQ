5500454ac7c1c455ad0e72db2e2fe89fc8b55f3e
Giannis Nikolopoulos <el15170@mail.ntua.gr>
2023-01-06 18:49:08 +0200

    Moved view template

--- frontend/intelliq-frontend/src/views/ResponderQuestionnaires.vue
+++ frontend/intelliq-frontend/src/views/ResponderQuestionnaires.vue
@@ -1,3 +1,19 @@
+<template>
+    <div>
+        <h2>Available questionnaires:</h2>
+        <table>
+            <tr v-for="q in questions">
+                <td>
+                    {{ q[1] }}
+                </td>
+                <td>
+                    <router-link :to="'/answer/' + q[0]">Answer!</router-link>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
 <script>
 import { RouterLink } from 'vue-router';
 import axios from 'axios';
@@ -11,7 +27,6 @@ export default {
     },
     methods: {
         async getQuestions() {
-            console.log("Getting Questions")
             axios.get("http://127.0.0.1:9103/getQuestionnaires",
                 { headers: { "X-OBSERVATORY-AUTH": "e00f8e21a864de304a6c" } }).then(
                     (response) => {
@@ -21,23 +36,7 @@ export default {
         }
     },
     async created() {
-        console.log('mounting');
         this.getQuestions();
     }
 }
 </script>
-<template>
-    <div>
-        <div>Available questionnaires:</div>
-        <table>
-            <tr v-for="q in questions">
-                <td>
-                    {{ q[1] }}
-                </td>
-                <td>
-                    <router-link :to="'/answer/' + q[0]">Answer!</router-link>
-                </td>
-            </tr>
-        </table>
-    </div>
-</template>
