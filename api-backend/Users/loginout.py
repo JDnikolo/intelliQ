33f72bc2a7334dbfce3b9b154123f9201bebbb63
Giannis Nikolopoulos <el15170@mail.ntua.gr>
2023-01-13 12:12:00 +0200

    Added TODOs to consider

--- api-backend/Users/loginout.py
+++ api-backend/Users/loginout.py
@@ -15,11 +15,11 @@ def loginf():
     if request.headers.get("Content-Type") != "application/x-www-form-urlencoded":
         # invalid content type for logging in
         return jsonify({
-                "type":"/errors/authentication-error",
-                "title": "Bad Request",
-                "status": "400",
-                "detail":"Credentials must be x-www-form-urlencoded.",
-                "instance":"/login"}), 400
+            "type": "/errors/authentication-error",
+            "title": "Bad Request",
+            "status": "400",
+            "detail": "Credentials must be x-www-form-urlencoded.",
+            "instance": "/login"}), 400
     sqlcursor = myconnector.cursor()
     username = request.form["username"]
     password = request.form["password"]
@@ -30,14 +30,14 @@ def loginf():
     if len(result) == 0:
         # either password or username was incorrect,
         return jsonify({
-                "type":"/errors/authentication-error",
-                "title": "Unauthorized",
-                "status": "401",
-                "detail":"Invalid Credentials",
-                "instance":"/login"}), 401
+            "type": "/errors/authentication-error",
+            "title": "Unauthorized",
+            "status": "401",
+            "detail": "Invalid Credentials",
+            "instance": "/login"}), 401
     if result[0][0] == None or result[0][0] == "":
-        uid = uuid4().hex[:30]
         # user isn't already logged in, create access token and return it
+        uid = uuid4().hex[:30]
         sqlcursor.execute(
             "UPDATE Users SET access_token=%s WHERE username=%s AND us_password=%s",
             [uid, username, password])
@@ -45,24 +45,26 @@ def loginf():
         sqlcursor.close()
         return {"token": uid}, 200
     else:
+        # user is already logged in
+        # TODO: consider changing behavior here
         sqlcursor.close()
         return jsonify({
-                "type":"/errors/authentication-error",
-                "title": "Conflict",
-                "status": "400",
-                "detail":"Already logged in.",
-                "instance":"/login"}), 400      # insted of 409:conflict error
+            "type": "/errors/authentication-error",
+            "title": "Conflict",
+            "status": "400",
+            "detail": "Already logged in.",
+            "instance": "/login"}), 400      # insted of 409:conflict error
 
 
 @logout.route("/logout", methods=["POST"])
 def logoutf():
     if not (authUser()):
         return jsonify({
-                "type":"/errors/authentication-error",
-                "title": "Unauthorized",
-                "status": "401",
-                "detail":"Invalid or missing access token.",
-                "instance":"/logout"}), 401     # could be 400: Bad request 
+            "type": "/errors/authentication-error",
+            "title": "Unauthorized",
+            "status": "401",
+            "detail": "Invalid or missing access token.",
+            "instance": "/logout"}), 401     # could be 400: Bad request
     else:
         uid = request.headers.get("X-OBSERVATORY-AUTH")
         sqlcursor = myconnector.cursor()
