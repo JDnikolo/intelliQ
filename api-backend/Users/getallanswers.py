4b5aee258a16f187cb16c80d3486eac607b9fa97
ntua-el19156 <el19156@mail.ntua.gr>
2023-01-17 13:23:38 +0200

    getallanswers bug fix
    Τώρα δουλεύει σωστά ακόμα κι αν υπάρχει κενό στην αρχή ή στο τέλος.

--- api-backend/Users/getallanswers.py
+++ api-backend/Users/getallanswers.py
@@ -60,6 +60,22 @@ def getallanswers(questionnaireID):
                             "detail": "No answers were found.",
                             "instance": "/Users/getallanswers"}), 402
             sqlcursor.close()
+            # Change result so that it has all questions and no answers, even with NO ANSWER
+            p = 0 # pointer
+            i = 0
+            j = 0
+            for i in range(len(questions)):
+                for j in range(len(sessions)):
+                    print(p)
+                    if(p < len(result) and (not result[p][0] == questions[i][0] or not result[p][1] == sessions[j][0])):
+                        # Question Session does not exist, add it with NO ANSWER
+                        result.insert(p, (questions[i][0], sessions[j][0], "NO ANSWER"))
+                    p = p + 1
+            # If p <= len(sessions)*len(questions), some NO ANSWER must be appended
+            if (p <= len(sessions)*len(questions)):
+                for ii in range(i, len(questions)):
+                    for jj in range(j, len(sessions)):
+                        result.append((questions[ii][0], sessions[jj][0], "NO ANSWER"))
             
             temp_result = [] #Initialize list to store answers of each question
             for i in range(len(questions)):
@@ -69,12 +85,25 @@ def getallanswers(questionnaireID):
             j = 0 #I declare j here because I need it outside its loop
             pointer = 0 #location in result
             current_qid = result[0][0]
+            
+            # Correct way to make dictionairies
             for i in range(len(questions)):
+                for j in range(len(sessions)):
+                    print("pointer = ", pointer)
+                    print("q = ", questions[i][0])
+                    print("s = ", sessions[j][0])
+                    temp_result[i].append(dict(session = sessions[j][0], ans = result[pointer][2]))
+                    pointer = pointer + 1
+                total_result.append(dict(questionID = questions[i][0], answers = temp_result[i]))
+            # The following block is replaced by block above
+            '''for i in range(len(questions)):
                 k = 0
-                if (result[pointer][0] == current_qid and pointer != 0):
+                #pointer < len is new addition
+                if (pointer < len(result) and result[pointer][0] == current_qid and pointer != 0):
                     pointer += 1
                     current_qid = result[pointer][0]
-                else:
+                #else:
+                elif (pointer < len(result)):
                     current_qid = result[pointer][0]
                 for j in range(len(sessions)):
                     if (pointer + k >= len(result) and k == len(sessions)):
@@ -84,7 +113,8 @@ def getallanswers(questionnaireID):
                             temp_result[i].append(dict(session = sessions[k][0], ans = "NO ANSWER"))
                             k += 1
                         break
-                    if (result[pointer + j][1] == sessions[k][0]): #Q has answer for current session
+                    #print(k)
+                    if (k < len(sessions) and result[pointer + j][1] == sessions[k][0]): #Q has answer for current session
                         temp_result[i].append(dict(session = result[pointer + j][1], ans = result[pointer + j][2]))
                         k += 1
                     else: #Q does not have answer for current session, NO ANSWER
@@ -93,7 +123,7 @@ def getallanswers(questionnaireID):
                             k += 1
                         j -= 1
                 pointer += j
-                total_result.append(dict(questionID = current_qid, answers = temp_result[i]))
+                total_result.append(dict(questionID = current_qid, answers = temp_result[i]))'''
             if (format == "json"):
                 return jsonify({"questionnaireID": str(questionnaireID),
                 "full_answers": total_result}), 200
